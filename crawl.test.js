eff0c5583e7ef4d2aa42e61bb035c19e7dc35df8
abishek-dev-max <abishek.devapal@gmail.com>
2023-04-15 18:29:00 +0530

    getURLfromhtml funtion

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,5 @@
-const {NormalizaURL} = require('./crawl.js')
+const { html } = require('parse5')
+const {NormalizaURL,getURLsFromHTML} = require('./crawl.js')
 const {test,expect} = require('@jest/globals')
 test('normalizeURl strip HTTPS Protocol',()=>{
     const input='https://blog.boot.dev/path'
@@ -22,5 +23,64 @@ test('normalizeURl strip HTTP protocol',()=>{
     const input='https://blog.boot.dev/path/'
     const actual=NormalizaURL(input)
     const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+test('getURLsFromHTMl',()=>{
+    const HTMLinput=`
+    <html>
+    <body>
+        <a href="https://blog.boot.dev/">
+            Boot.dev Blog
+        </a>
+    </body>
+    </html>`
+    const inputBaseURL="https://blog.boot.dev/path"
+    const actual=getURLsFromHTML(HTMLinput,inputBaseURL)
+    const expected = ['https://blog.boot.dev/']
+    expect(actual).toEqual(expected)
+})
+test('getURLsFromHTMl relative',()=>{
+    const HTMLinput=`
+    <html>
+    <body>
+        <a href="/path/">
+            Boot.dev Blog
+        </a>
+    </body>
+    </html>`
+    const inputBaseURL="https://blog.boot.dev"
+    const actual=getURLsFromHTML(HTMLinput,inputBaseURL)
+    const expected = ['https://blog.boot.dev/path/']
+    expect(actual).toEqual(expected)
+})
+test('getURLsFromHTMl multiple links',()=>{
+    const HTMLinput=`
+    <html>
+    <body>
+        <a href='https://blog.boot.dev/path1/'>
+            Boot.dev Blog Path One
+        </a>
+        <a href='https://blog.boot.dev/path2/'>
+        Boot.dev Blog Path Two
+    </a>
+    </body>
+    </html>`
+    const inputBaseURL="https://blog.boot.dev"
+    const actual=getURLsFromHTML(HTMLinput,inputBaseURL)
+    const expected = ['https://blog.boot.dev/path1/','https://blog.boot.dev/path2/']
+    expect(actual).toEqual(expected)
+})
+test('getURLsFromHTMl invlaid URl',()=>{
+    const HTMLinput=`
+    <html>
+    <body>
+        <a href="invalid">
+            Invalid Url
+        </a>
+    </body>
+    </html>`
+    const inputBaseURL="https://blog.boot.dev"
+    const actual=getURLsFromHTML(HTMLinput,inputBaseURL)
+    const expected = []
     expect(actual).toEqual(expected)
 })
